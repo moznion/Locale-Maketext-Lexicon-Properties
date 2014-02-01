1d029b3ae3cc35b7d395d3a8de9e31620d6f05d1
moznion <moznion@gmail.com>
2014-02-01 17:28:28 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Locale::Maketext::Lexicon::Properties - It's new $module
+
+# SYNOPSIS
+
+    use Locale::Maketext::Lexicon::Properties;
+
+# DESCRIPTION
+
+Locale::Maketext::Lexicon::Properties is ...
+
+# LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+moznion <moznion@gmail.com>
---
+++ lib/Locale/Maketext/Lexicon/Properties.pm
@@ -0,0 +1,39 @@
+package Locale::Maketext::Lexicon::Properties;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Locale::Maketext::Lexicon::Properties - It's new $module
+
+=head1 SYNOPSIS
+
+    use Locale::Maketext::Lexicon::Properties;
+
+=head1 DESCRIPTION
+
+Locale::Maketext::Lexicon::Properties is ...
+
+=head1 LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+moznion E<lt>moznion@gmail.comE<gt>
+
+=cut
+
