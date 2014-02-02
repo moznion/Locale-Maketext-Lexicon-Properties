1b1f0f4cad7663d00d10f67c7f40b9be74ae3c21
moznion <moznion@gmail.com>
2014-02-02 19:45:33 +0900

    Improve docs

--- lib/Locale/Maketext/Lexicon/Properties.pm
+++ lib/Locale/Maketext/Lexicon/Properties.pm
@@ -61,9 +61,14 @@ Directly calling C<Locale::Maketext::Lexicon::Properties::parse()>:
 
 =head1 DESCRIPTION
 
-This module parses .properties file (from Java) for L<Locale::Maketext> by using L<Locale::Maketext::Lexicon>. And it can also returns a Lexicon hash.
+This module parses the properties file (from Java) for L<Locale::Maketext> by using L<Locale::Maketext::Lexicon>. And it can also return a Lexicon hash.
 
-You can lookup the property value by specifying key to C<maketext()> or Lexcon hash.
+You are able to look up the property value by specifying key to C<maketext()> or Lexcon hash.
+
+=head1 NOTES
+
+Properties file can use colon (:) as delimiter as an alternative to equal (=), however this module cannot.
+And properties file allows multi-line property, but this module cannot handle it.
 
 =head1 SEE ALSO
 
